94e22155e02f7371de77f1505d473a92b79ac9c4
Tomasema <tomas.martinez@syloper.com>
2023-08-05 15:00:08 -0300

    feat: reconocimientos de importaciones con paths

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 // Views
 import { HomeComponent } from './home.component';
-import { ProductsComponent } from './views/products/products.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductsComponent } from '@home/views';
+// Components
+import { ProductCardComponent } from '@home/components';
 // Shared
-import { NavbarComponent } from 'src/app/shared/components';
+import { NavbarComponent } from '@shared/components';
 // Material Imports
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
@@ -19,12 +20,16 @@ import { MatButtonModule } from '@angular/material/button';
     // Views
     HomeComponent,
     ProductsComponent,
+    // Components
     ProductCardComponent
   ],
   imports: [
+    // Angular
     CommonModule,
     HomeRoutingModule,
+    // Shared
     NavbarComponent,
+    // Material
     MatIconModule,
     MatCardModule,
     MatButtonModule
